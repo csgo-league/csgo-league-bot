c5c500b5c382f84385f845325b36d6dd709a7271
cameronshinn <cameron.shinn@hotmail.com>
2020-03-28 11:28:31 -0700

    Remove bot generic option

--- qbot/qbot.py
+++ qbot/qbot.py
@@ -7,7 +7,7 @@ BOT_COLOR = 0x0D61B7
 DATA_PATH = 'guild_data.json'
 
 
-def run(discord_token, dbl_token=None, donate_url=None, generic=False):
+def run(discord_token, dbl_token=None, donate_url=None):
     """ Create the bot, add the cogs and run it. """
     bot = commands.Bot(command_prefix=('q!', 'Q!'), case_insensitive=True)
     bot.add_cog(cogs.CacherCog(bot, DATA_PATH))
@@ -15,11 +15,8 @@ def run(discord_token, dbl_token=None, donate_url=None, generic=False):
     bot.add_cog(cogs.HelpCog(bot, BOT_COLOR))
     bot.add_cog(cogs.QueueCog(bot, BOT_COLOR))
     bot.add_cog(cogs.TeamDraftCog(bot, BOT_COLOR))
-
-    if not generic:
-        bot.add_cog(cogs.MapDraftCog(bot, BOT_COLOR))
-        bot.add_cog(cogs.PopflashCog(bot, BOT_COLOR))
-        bot.remove_command('cap')
+    bot.add_cog(cogs.MapDraftCog(bot, BOT_COLOR))
+    bot.add_cog(cogs.PopflashCog(bot, BOT_COLOR))
 
     if dbl_token:
         bot.add_cog(cogs.DblCog(bot, dbl_token))
