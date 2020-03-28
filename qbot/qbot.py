782cf846b17ca1e47ced455a9c9629bc96085f75
cameronshinn <cameron.shinn@hotmail.com>
2020-03-28 15:46:16 -0700

    Reorder cog load for help menu

--- qbot/qbot.py
+++ qbot/qbot.py
@@ -15,10 +15,10 @@ def run(discord_token, api_base_url, api_key, dbl_token=None, donate_url=None):
     bot.add_cog(cogs.CacherCog(bot, DATA_PATH))
     bot.add_cog(cogs.ConsoleCog(bot))
     bot.add_cog(cogs.HelpCog(bot, BOT_COLOR))
+    bot.add_cog(cogs.AuthCog(bot, api_helper, BOT_COLOR))
     bot.add_cog(cogs.QueueCog(bot, api_helper, BOT_COLOR))
     bot.add_cog(cogs.TeamDraftCog(bot, BOT_COLOR))
     bot.add_cog(cogs.MapDraftCog(bot, BOT_COLOR))
-    bot.add_cog(cogs.AuthCog(bot, api_helper, BOT_COLOR))
 
     if dbl_token:
         bot.add_cog(cogs.DblCog(bot, dbl_token))
