b5b53c080d81e78689c7ea749653b632e852a79d
cameronshinn <cameron.shinn@hotmail.com>
2020-03-29 00:27:32 -0700

    Ignore linting long line we will resolve soon

--- qbot/cogs/queue.py
+++ qbot/cogs/queue.py
@@ -108,7 +108,7 @@ class QueueCog(commands.Cog):
             random.shuffle(shuffled_players)
             team_one = shuffled_players[:team_size]
             team_two = shuffled_players[team_size:]
-            # team_one, team_two = [shuffled_players[i * team_size:(i + 1) * team_size] for i in range((len(shuffled_players) + team_size - 1) // team_size)]
+            # team_one, team_two = [shuffled_players[i * team_size:(i + 1) * team_size] for i in range((len(shuffled_players) + team_size - 1) // team_size)]  # noqa
             match = self.api_helper.start_match(team_one, team_two)
 
             if match:
