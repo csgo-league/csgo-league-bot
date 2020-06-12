cc3a02e3dfbe6f09939612d30c108ac3b0fd702c
Cameron Shinn <cameron.shinn@hotmail.com>
2020-06-12 09:15:44 -0700

    Adhere to PEP8

--- bot/cogs/match.py
+++ bot/cogs/match.py
@@ -169,7 +169,7 @@ class TeamDraftMenu(discord.Message):
         if captain_method == 'volunteer':
             for emoji in self.pick_emojis:
                 await self.add_reaction(emoji)
-        else: # don't add reaction for captains
+        else:  # Don't add reaction for captains
             items = self.pick_emojis.items()
             for emoji, member in items:
                 if member in self.members_left:
