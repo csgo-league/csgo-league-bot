3b4356ea4ebdff5ce8a5ef14e4a656e9d1c98b1a
cameronshinn <cameron.shinn@hotmail.com>
2020-07-24 08:33:52 -0700

    Account for "/" at the end of the API URL

--- launcher.py
+++ launcher.py
@@ -5,23 +5,27 @@ from bot.bot import LeagueBot
 import asyncio
 import asyncpg
 from dotenv import load_dotenv
-from operator import itemgetter
-from os import environ
+import os
+
+load_dotenv()  # Load the environment variables in the local .env file
 
 
 def run_bot():
     """ Parse the config file and run the bot. """
-    # Load the environment variables in the local .env file
-    load_dotenv()
-
     # Get database object for bot
     connect_url = 'postgresql://{POSTGRESQL_USER}:{POSTGRESQL_PASSWORD}@{POSTGRESQL_HOST}/{POSTGRESQL_DB}'
     loop = asyncio.get_event_loop()
-    db_pool = loop.run_until_complete(asyncpg.create_pool(connect_url.format(**environ)))
+    db_pool = loop.run_until_complete(asyncpg.create_pool(connect_url.format(**os.environ)))
 
-    # Instantiate bot and run
-    env_varnames = ['DISCORD_BOT_TOKEN', 'CSGO_LEAGUE_API_URL', 'CSGO_LEAGUE_API_KEY']
-    bot = LeagueBot(*itemgetter(*env_varnames)(environ), db_pool=db_pool)
+    # Get environment variables
+    bot_token = os.environ['DISCORD_BOT_TOKEN']
+    api_url = os.environ['CSGO_LEAGUE_API_URL']
+    api_key = os.environ['CSGO_LEAGUE_API_KEY']
+
+    if api_url.endswith('/'):
+        api_url = api_url[:-1]
+
+    bot = LeagueBot(bot_token, api_url, api_key, db_pool)
     bot.run()
 
 
