517c959e22a2295a9d003e0fa706929fcd5e7b19
cameronshinn <cameron.shinn@hotmail.com>
2020-08-04 21:11:18 -0700

    Change logging to use config dict

--- bot/cogs/logging.py
+++ bot/cogs/logging.py
@@ -3,31 +3,70 @@
 import __main__
 from discord.ext import commands
 import logging
-from logging import handlers
+from logging import config
 from os import path
-import sys
-
-LOGFILE = path.join(path.dirname(path.abspath(__main__.__file__)), 'bot.log')  # Log file in root directory
 
 
-def format_dec(log_func):
+LOGGING_CONFIG = {
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s][%(name)s][%(levelname)s] %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'defaultNoDate': {
+            'format': '[%(asctime)s][%(name)s][%(levelname)s] %(message)s',
+            'datefmt': '%H:%M:%S'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'defaultNoDate',
+            'level': 'INFO',
+            'stream': 'ext://sys.stdout'
+        },
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'default',
+            'level': 'DEBUG',
+            'filename': path.join(path.dirname(path.abspath(__main__.__file__)), 'bot.log'),
+            'maxBytes': 7340032,
+            'encoding': 'utf-8'
+        }
+    },
+    'loggers': {
+        'csgoleague': {
+            'level': 'DEBUG'
+        },
+        'discord.client': {
+            'level': 'INFO'
+        },
+        'discord.gateway': {
+            'level': 'WARNING'
+        }
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': [
+            'console',
+            'file'
+        ]
+    }
+}
+
+config.dictConfig(LOGGING_CONFIG)
+
+
+def log_lines(lvl, msg, *args, sub_lines=None, **kwargs):
     """"""
-    def log_formatted(*args, **kwargs):
-        """"""
-        msg = args[1]
-        sub_lines = kwargs.get('sub_lines', None)
+    if sub_lines is not None:
+        longest_subl_pref = len(max(sub_lines.keys(), key=len))
 
-        if sub_lines is not None:
-            longest_subl_pref = len(max(sub_lines.keys(), key=len))
+        for prefix, suffix in sub_lines.items():
+            msg += '\n    {:<{width}} {}'.format(prefix + ':', suffix, width=longest_subl_pref + 1)
 
-            for prefix, suffix in sub_lines.items():
-                msg += '\n    {:<{width}} {}'.format(prefix + ':', suffix, width=longest_subl_pref + 1)
-
-        new_args = list(args)
-        new_args[1] = msg
-        log_func(*new_args, **kwargs)
-
-    return log_formatted
+    logging.getLogger('csgoleague.bot').log(lvl, msg, *args, **kwargs)
 
 
 class LoggingCog(commands.Cog):
@@ -36,73 +75,35 @@ class LoggingCog(commands.Cog):
     def __init__(self, bot):
         """ Set bot attribute. """
         self.bot = bot
-
-        # Get logger
-        logger = logging.getLogger('csgoleague')
-        logger.setLevel(logging.INFO)
-
-        # Add stdout handler
-        base_fmt_str = '[{asctime}][{levelname}]: {message}'
-        sformatter = logging.Formatter(fmt=base_fmt_str, datefmt='%H:%M:%S', style='{')
-        shandler = logging.StreamHandler(sys.stdout)
-        shandler.setFormatter(sformatter)
-        logger.addHandler(shandler)
-
-        # Add rotating file handler
-        fformatter = logging.Formatter(fmt=base_fmt_str, datefmt='%Y-%m-%d %H:%M:%S', style='{')
-        fhandler = handlers.RotatingFileHandler(LOGFILE, maxBytes=7340032, encoding='utf-8')  # 7MB size
-        fhandler.setFormatter(fformatter)
-        logger.addHandler(fhandler)
-
-        self.logger = logger
-
-    @format_dec
-    def debug(self, msg, sub_lines=None):
-        self.logger.debug(msg)
-
-    @format_dec
-    def info(self, msg, sub_lines=None):
-        self.logger.info(msg)
-
-    @format_dec
-    def warning(self, msg, sub_lines=None):
-        self.logger.warning(msg)
-
-    @format_dec
-    def error(self, msg, sub_lines=None):
-        self.logger.error(msg)
-
-    @format_dec
-    def critical(self, msg, sub_lines=None):
-        self.logger.critical(msg)
+        self.logger = logging.getLogger('csgoleague.bot')
 
     @commands.Cog.listener()
     async def on_connect(self):
         lines_dict = {'Username': self.bot.user.name, 'ID': self.bot.user.id}
-        self.info('Connected to Discord', sub_lines=lines_dict)
+        log_lines(logging.INFO, 'Connected to Discord', sub_lines=lines_dict)
 
     @commands.Cog.listener()
     async def on_disconnect(self):
-        self.info('Disconnected from Discord')
+        log_lines(logging.INFO, 'Disconnected from Discord')
 
     @commands.Cog.listener()
     async def on_resumed(self):
         lines_dict = {'Username': self.bot.user.name, 'ID': self.bot.user.id}
-        self.info('Resumed session with Discord', sub_lines=lines_dict)
+        log_lines(logging.INFO, 'Resumed session with Discord', sub_lines=lines_dict)
 
     @commands.Cog.listener()
     async def on_ready(self):
-        self.info(f'Bot is ready to use in {len(self.bot.guilds)} Discord servers')
+        log_lines(logging.INFO, 'Bot is ready to use in %s Discord servers', len(self.bot.guilds))
 
     @commands.Cog.listener()
     async def on_command(self, ctx):
         lines_dict = {'Caller': f'{ctx.author} ({ctx.author.id})', 'Guild': f'{ctx.guild} ({ctx.guild.id})'}
-        self.info(f'Command "{ctx.command}" issued', sub_lines=lines_dict)
+        log_lines(logging.INFO, 'Command "%s" issued', ctx.command, sub_lines=lines_dict)
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        self.info('Bot has been added to server "{guild.name}" ({guild.id})')
+        log_lines(logging.INFO, 'Bot has been added to server "%s" (%s)', guild.name, guild.id)
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        self.info('Bot has been removed from server "{guild.name}" ({guild.id})')
+        log_lines(logging.INFO, 'Bot has been removed from server "%s" (%s)', guild.name, guild.id)
