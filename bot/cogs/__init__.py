efd52f895f1f1cbb1bb0d6f0c2586d5a7d3ef598
Cameron Shinn <cameron.shinn@hotmail.com>
2020-09-25 23:31:08 -0700

    Remove __all__ declaration

--- bot/cogs/__init__.py
+++ bot/cogs/__init__.py
@@ -1,5 +1,6 @@
 # __init__.py
 
+from . import utils
 from .auth import AuthCog
 from .logging import LoggingCog
 from .donate import DonateCog
@@ -7,13 +8,3 @@ from .help import HelpCog
 from .queue import QueueCog
 from .stats import StatsCog
 from .match import MatchCog
-
-__all__ = [
-    AuthCog,
-    LoggingCog,
-    DonateCog,
-    HelpCog,
-    QueueCog,
-    StatsCog,
-    MatchCog
-]
