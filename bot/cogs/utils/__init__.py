a2d25c8f68506f6edc7d6879759d295299572455
Cameron Shinn <cameron.shinn@hotmail.com>
2020-10-04 20:46:11 -0700

    Correct LeagueContext name

--- bot/cogs/utils/__init__.py
+++ bot/cogs/utils/__init__.py
@@ -1,7 +1,7 @@
 # __init__.py
 
 from .api import ApiWrapper
-from .context import LeagueBotContext
+from .context import LeagueContext
 from .db import DBHelper
 from .map import Map, MapPool
 from .player import Player
