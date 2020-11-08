15e9dfdb7faa20180d050fcdfcaf7cd0fed14c26
Cameron Shinn <cameron.shinn@hotmail.com>
2020-11-08 01:45:29 -0800

    Import MatchServer from correct file

--- bot/cogs/utils/__init__.py
+++ bot/cogs/utils/__init__.py
@@ -1,7 +1,7 @@
 # __init__.py
 
-from .api import MatchServer
 from .context import LeagueContext
 from .db import DBHelper
 from .map import Map, MapPool
 from .player import Player, PlayerStats
+from .server import MatchServer
