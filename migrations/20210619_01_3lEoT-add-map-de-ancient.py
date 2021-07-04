ada6e6966c6f8260d5ac7c0aec5587865aee1679
cameronshinn <cameron.shinn@hotmail.com>
2021-07-03 20:23:35 -0700

    Correct string formatting

--- migrations/20210619_01_3lEoT-add-map-de-ancient.py
+++ migrations/20210619_01_3lEoT-add-map-de-ancient.py
@@ -9,11 +9,11 @@ __depends__ = {'20200621_01_XkKXW-add-map-draft-columns'}
 steps = [
     step(
         (
-            'ALTER TABLE guilds\n',
+            'ALTER TABLE guilds\n'
             'ADD COLUMN de_ancient BOOL NOT NULL DEFAULT true;'
         ),
         (
-            'ALTER TABLE guilds\n',
+            'ALTER TABLE guilds\n'
             'DROP COLUMN de_ancient;'
         )
     )
